d1c8ab5868733e1ca52d7469719f7b93becc8a19
pixtur <pixtur@1bdc3648-5f13-0410-827e-9003b32a0c15>
2008-05-31 20:49:08 +0000

    - fixed render wiki bug when link with pipe above table - added user setting to ignore filtering of own changes in recent changes lists - updated jquery and jeditable versions to prepare for request feedback ajax features - fixed several minor typos - checked for missing iconf-function when exporting csv

--- js/misc.js
+++ js/misc.js
@@ -16,6 +16,9 @@
 var onLoadFunctions= new Array();
 var ajax_edits= new Array();
 
+/**
+* called after loading
+*/
 function misc()
 {
 
@@ -109,29 +112,23 @@ function misc()
           $('.tab_header', group).each(function(){
             this.group = group;
             $(this).click(f);
-            //$('#'+this.id+'-body').hide();
             $('#'+this.id+'-body').addClass('Hidden');
           }).filter(':first').trigger('click');
         });
         $('.tabgroup ul li:first').addClass('Active');
         $('.tabgroup div:first').removeClass('Hidden');
       },
-      click: function(e) {
-        //alert("click("+this.id+")");
-        //alert("removeHidden from"+ tab);
 
+      click: function(e) {
         var tab = $('#'+this.id+'-body').get(0);
 
         $('.tab_header', this.group).each(function(){
           $(this).removeClass('Active');
-          //$('#'+this.id+'-body').hide();
           $('#'+this.id+'-body').addClass('Hidden');
         });
         $(this).addClass('Active');
-        //$(tab).show();
 
         $(tab).removeClass('Hidden');
-        //this.blur();
         e.preventDefault();
       }
     };
@@ -249,25 +246,20 @@ function getMoreChanges(project, start, count)
 {
     var element= $('ul#changesOnProject_' + project);
 
-    if(!this.starts)
-    {
+    // initialize or update static variables
+    if(!this.starts) {
         this.starts = Array();
     }       
         
-    if(this.starts[project])
-    {
+    if(this.starts[project]) {
         start= this.starts[project] + count;
     }
     this.starts[project] = start;
-    
-    
             
     $.get("index.php",
         { go:'ajaxMoreChanges', start: start, count: count, prj:project },
         function(data) {
             $(element).append(data);
-            
-            //alert("start="+ start+ " count=" + count);
         }
     );
     return;
@@ -276,6 +268,8 @@ function getMoreChanges(project, start, count)
 
 /**
 * add inline edit buttons for wiki chapters
+* 
+* read more documentation at http://www.streber-pm.org/3695
 */
 function AjaxEdit(dom_element, item_id, field)
 {
@@ -321,17 +315,21 @@ function AjaxEdit(dom_element, item_id, field)
             $(this).attr('title', 'Doubleclick to edit chapter');
 
             $(this).editable('index.php?go=itemSaveField&item=' + item_id + '&field=' + field_name + '&chapter=' + chapter_name, {
-                postload:'index.php?go=itemLoadField&item=' + item_id + '&field=' + field_name + '&chapter=' + chapter_name,
+                loadurl:'index.php?go=itemLoadField&item=' + item_id + '&field=' + field_name + '&chapter=' + chapter_name,
                 type:'textarea',
                 submit:'Save',
                 cancel:'Cancel',
                 chapter:true,
-                obj:dom_element
+                obj:dom_element,
+                placeholder:'',
+                event:'dblclick',
+                callback:function(value, settings){
+                  keepParentNode = this.parentNode
+                  $(this.parentNode).html( value );
+                  keepParentNode.ajax_edit.initEditChapters();
+                }
             });
         });
     }
     this.initEditChapters();
 }
-
-
-
