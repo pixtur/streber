d3c21410fccb3ff7ed67ea969c093f3afe53f430
Thomas Mann <thomas.mann@framefield.com>
2014-09-19 00:58:04 +0200

    no message

--- pages/topic_export_as_html.inc.php
+++ pages/topic_export_as_html.inc.php
@@ -52,9 +52,10 @@ function TopicExportAsHtml()
         $complete_buffer .= ("<h1>" . $t->name . "</h1>");
         
         $wiki_as_html = wikifieldAsHtml($t, 'description');
-        $with_local_links = preg_replace("/href='/", "href='" . $t->getUrl() , $wiki_as_html);
+        //$with_local_links = preg_replace("/href='/", "href='" . $t->getUrl() , $wiki_as_html);
 
-        $complete_buffer .= $with_local_links;
+        //$complete_buffer .= $with_local_links;
+        $complete_buffer .= $wiki_as_html;
     }
     $complete_buffer.= "</body></html>";
 
